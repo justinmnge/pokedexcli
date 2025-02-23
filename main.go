e218d745cff488455d4d2ccd19f2d106b929a5dd
Justin Monge <justin.mnge@gmail.com>
2025-02-23 13:47:17 -0400

    feat ✨: created a help and exit command

--- main.go
+++ main.go
@@ -13,15 +13,62 @@ func cleanInput(text string) []string {
 	return strings.Fields(cleaned)
 }
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+
 	for {
 		fmt.Println("Pokedex > ")
 		scanner.Scan()          // this waits for user input
 		input := scanner.Text() // this gets the text they typed
 		cleanedInput := cleanInput(input)
-		if len(cleanedInput) > 0 {
-			fmt.Printf("Your command was: %s\n", cleanedInput[0])
+		if len(cleanedInput) == 0 {
+			continue
+		}
+
+		if cmd, ok := commands[cleanedInput[0]]; ok {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
 		}
 	}
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
